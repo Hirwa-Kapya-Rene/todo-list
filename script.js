e7591ad295baae604c9c0e3741ac7a55e0aae549
Hirwa Rene <hirwarene6@gmail.com>
2025-03-09 12:00:47 +0200

    added onclick function on button in html

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 const inputBox = document.querySelector('#inputBox');
-const container = document.querySelector('#inputBox');
+const listContainer = document.querySelector('#list-container');
+function addTask(){
+  if(inputBox.value === ''){
+    alert('Please enter a task');
+  }
+  else{
+    let li =document.createElement('li');
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement('span');
+    span.innerHTML = '\u00D7';
+    li.appendChild(span);
+  }
+  inputBox.value = '';
+  saveData();
+}
+listContainer.addEventListener('click', function(e){
+  if(e.target.tagName === 'LI'){
+    e.target.classList.toggle('checked');
+    saveData();
+  }
+  else if(e.target.tagName === 'SPAN'){
+    e.target.parentElement.remove();
+    saveData();
+  }
+  
+}, false
+);
+
+function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTasks(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTasks();
